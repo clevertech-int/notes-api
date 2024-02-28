f2daff01cb8d8915123dd1c4f9d89c0c71ae7d6c
Jacek Lawniczak <jacekelgda@gmail.com>
2024-02-28 17:55:27 +0100

    wip

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ const httpsOptions = {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { httpsOptions });
   await app.listen(8081);
+
   console.log(`Running on port: ${await app.getUrl()}`);
 }
 bootstrap();
