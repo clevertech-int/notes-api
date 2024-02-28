61b26ea988bcc033fcd5f43ac724b1a70e0fa0d2
Jacek Lawniczak <jacekelgda@gmail.com>
2024-02-28 12:11:59 +0100

    deploy

--- src/redis-client/redis-client.service.ts
+++ src/redis-client/redis-client.service.ts
@@ -1,18 +1,18 @@
-import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Client } from 'redis-om';
-import { createClient } from 'redis';
 
 @Injectable()
-export class RedisClientService extends Client implements OnModuleDestroy {
+export class RedisClientService
+  extends Client
+  implements OnModuleDestroy, OnModuleInit
+{
   constructor(private readonly configService: ConfigService) {
     super();
-    // (async () => {
-    // const connection = createClient();
-    // await connection.connect();
-    // this.use(connection);
-    // this.open();
-    // })();
+  }
+
+  public async onModuleInit() {
+    await this.open(this.configService.get<string>('REDIS_CLOUD_URL'));
   }
 
   public async onModuleDestroy() {
