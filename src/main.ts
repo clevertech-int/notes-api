f28a4c9eac9140ae5778589916ec7137b4bb318c
Jacek Lawniczak <jacekelgda@gmail.com>
2024-02-28 18:32:32 +0100

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as fs from 'fs';
-const httpsOptions = {
-  key: fs.readFileSync('./secrets/server.key'),
-  cert: fs.readFileSync('./secrets/server.cert'),
-};
+// import * as fs from 'fs';
+// const httpsOptions = {
+//   key: fs.readFileSync('./secrets/server.key'),
+//   cert: fs.readFileSync('./secrets/server.cert'),
+// };
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { httpsOptions });
+  const app = await NestFactory.create(AppModule);
   await app.listen(8081);
 
   console.log(`Running on port: ${await app.getUrl()}`);
